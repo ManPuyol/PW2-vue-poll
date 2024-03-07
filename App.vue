2e081ca2a9aa70796b46e0a07c961168886bbfa5
dperezalvarez2 <dperezalvarez@viacoreit.com>
2024-03-07 01:11:40 -0800

    Vue poll

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+      <h1>¿Cuál es tu lenguaje de programación favorito?</h1>
+      <Poll />
+    </div>
+  </template>
+  
+  <script>
+  import Poll from './Poll.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      Poll
+    }
+  }
+  </script>
+  
---
+++ Poll.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <div v-for="(votes, option) in pollOptions" :key="option">
+        <button @click="addVote(option)">{{ option }}</button>
+        <span> {{ votes }} votos</span>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { reactive } from 'vue';
+  
+  const pollOptions = reactive({
+    JavaScript: 0,
+    Python: 0,
+    Ruby: 0,
+    Rust: 0,
+  });
+  
+//TODO.........
+  </script>
+  
